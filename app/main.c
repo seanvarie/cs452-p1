dfa5f2ca1276b16dd0277d84191113ba95992309
seanvarie <54995501+seanvarie@users.noreply.github.com>
2024-09-15 21:05:28 +0000

    print version complete and methods stubbed out in lab.c

--- app/main.c
+++ app/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include "../src/lab.h"
 
-int main(void)
+int main(int argc, char * argv[])
 {
-  printf("hello world\n");
+  int opt = -1;
+
+  while((opt = getopt(argc, argv, "v")) != -1)
+  {
+    switch (opt)
+    {
+      case 'v':
+        printf("Simple Shell version %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+      break;
+    }
+  }
   return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,3 +1,51 @@
-/**Update this file with the starter code**/
+#include "lab.h"
 
-//TODO: implement methods from lab.h
+char *get_prompt(const char *env)
+{
+    return env;
+    //TODO: implement
+}
+
+int change_dir(char **dir)
+{
+    return 0;
+    //TODO: implement
+}
+
+char **cmd_parse(char const *line)
+{
+    return &line;
+    //TODO: implement
+}
+
+void cmd_free(char ** line)
+{
+    //TODO: implement
+}
+
+char *trim_white(char *line)
+{
+    return line;
+    //TODO: implement
+}
+
+bool do_builtin(struct shell *sh, char **argv)
+{
+    return false;
+    //TODO: implement
+}
+
+void sh_init(struct shell *sh)
+{
+    //TODO: implement
+}
+
+void sh_destroy(struct shell *sh)
+{
+    //TODO: implement
+}
+
+void parse_args(int argc, char **argv)
+{
+    //TODO: implement
+}
